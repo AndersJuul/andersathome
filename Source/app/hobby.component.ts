3b60e91450310b139c54bcb620d21231e6b28db2
Anders Juul <anders_privat@hotmail.com>
2017-03-23 22:13:41 +0100

    a

---
+++ Source/app/hobby.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'hobby-component',
+    template: `
+    <div class="container">
+        <h2 class="well">Hobby-projekter!</h2>
+
+    </div>
+  `
+})
+export class HobbyComponent {
+}
--- Source/app/routes.ts
+++ Source/app/routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router'
 import { HomeComponent } from './home.component';
 import { BackupComponent } from './backup.component';
 import { ContactComponent } from './contact.component';
+import { HobbyComponent } from './hobby.component';
 
 export const appRoutes: Routes = [
 	{ path: 'backup', component: BackupComponent, pathMatch: 'full' },
 	{ path: 'kontakt', component: ContactComponent, pathMatch: 'full' },
+	{ path: 'hobby', component: HobbyComponent, pathMatch: 'full' },
 	{ path: '', component: HomeComponent, pathMatch: 'full' }
 ]
