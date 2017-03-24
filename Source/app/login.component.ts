16f17fedf6e2850de3244823cbaf9d736e3e8ceb
Anders Juul <aju@vertica.dk>
2017-03-24 14:52:21 +0100

    a

--- Source/app/login.component.ts
+++ Source/app/login.component.ts
@@ -4,21 +4,29 @@ import { Router } from '@angular/router'
 
 @Component({
     selector: 'login-component',
+    styles: [`
+        em { float: right; color:#E05C65; padding-left:10px;}
+    `    
+    ],
     template: `
         <h1>Login</h1>
         <hr>
         <div class="col-md-4">
-        <form #loginForm="ngForm" (ngSubmit)="login(loginForm.value)" autocomplete="off">
+        <form #loginForm="ngForm" (ngSubmit)="login(loginForm.value)" autocomplete="off" novalidate>
             <div class="form-group" >
             <label for="userName">User Name:</label>
-            <input (ngModel)="userName" name="userName" id="userName" type="text" class="form-control" placeholder="User Name..." />
+            <em *ngIf="loginForm.controls.userName?.invalid  && mouseoverLogin">Påkrævet</em>
+            <input (ngModel)="userName" name="userName" id="userName" type="text" class="form-control" placeholder="User Name..." required/>
             </div>
             <div class="form-group" >
             <label for="password">Password:</label>
-            <input (ngModel)="password" name="password" id="password" type="password" class="form-control"placeholder="Password..." />
+            <em *ngIf="loginForm.controls.password?.invalid && mouseoverLogin">Påkrævet</em>
+            <input (ngModel)="password" name="password" id="password" type="password" class="form-control"placeholder="Password..." required/>
             </div>
                 
-            <button type="submit" class="btn btn-primary">Login</button>
+            <span (mouseenter)="mouseoverLogin=true" (mouseleave)="mouseoverLogin=false">
+                <button type="submit" class="btn btn-primary" [disabled]="loginForm.invalid">Login</button>
+            </span>
             <button type="button" class="btn btn-default" (click)="cancel()">Cancel</button>
         </form>
         </div>  `
