986508afeef97590eb5b267e3e96819798571641
Anders Juul <aju@vertica.dk>
2017-03-31 08:31:02 +0200

    a

--- Source/app/auth.service.ts
+++ Source/app/auth.service.ts
@@ -6,6 +6,12 @@ export class AuthService {
     currentUser: IUser;
 
     loginUser(userName: string, password: string){
+        if(userName.toLowerCase()!=="anders" || password!==".Hanne1966")
+        {
+            this.currentUser = null;
+            return;
+        }
+
         this.currentUser={
             id:1,
             userName: userName,
