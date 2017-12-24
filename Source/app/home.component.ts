92f03b99b58f6f435bb639fced7ce1473be45900
AndersJuul <andersjuulsfirma@gmail.com>
2017-12-24 15:46:37 +0100

    a

--- Source/app/home.component.ts
+++ Source/app/home.component.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
         
         <p>Dette site er lavet til hobby-projekter, typisk teknik-afprøvning og gateway til iot i mit og pigernes hjem.</p>
         
-        <p>For tiden leger jeg med TeamCity, Octopus Deploy, Angular2/TypeScript, RabbitMq, REST APIs, IdentityServer3/OIDC og Micro Service, primært gennem s gennem 
+        <p>For tiden leger jeg med TeamCity, Octopus Deploy, Angular2/TypeScript, RabbitMq, REST APIs, IdentityServer3/OIDC og Micro Service, primært gennem  
         hobbyprojektet <a href="https://andersathome.dk/RideShare" target="_blank">RideShare</a>.</p>
         
         <p>Hjemmets maskiner er blevet samlet under Hyper-V på en enkelt Server 2016. Det hygges der med når børnene er kommet i seng :).</p>
