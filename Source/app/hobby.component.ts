36f66b35e286391a98792cc6953844b83a070ec2
AndersJuul <andersjuulsfirma@gmail.com>
2018-01-09 17:11:24 +0100

    a

--- Source/app/hobby.component.ts
+++ Source/app/hobby.component.ts
@@ -5,6 +5,10 @@ import { Component } from '@angular/core';
     template: `
     <div>
         <h2 class="well">Hobby-projekter!</h2>
+        <h3 class=well>IdentityServer</h3>
+        <a href="https://andersathome.dk/Ajf.IdentityServer3/Identity" target="_blank">IdentityServer 3. Kan benyttes efter aftale."</a>
+        <h3 class=well></h3>
+        <h3 class=well></h3>
 
     </div>
   `
